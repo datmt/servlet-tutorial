37a2d2d035547a9f8855b202dfe7f2e106fcbff9
dtt <t2dx.inc@gmail.com>
2022-08-16 10:31:52 +0700

    adding simple servlets

---
+++ jakarta-servlets-quick-start/src/main/java/com/datmt/servlet/DisplayOutput.java
@@ -0,0 +1,22 @@
+package com.datmt.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class DisplayOutput extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Simple Servlet response");
+	}
+
+
+
+}
